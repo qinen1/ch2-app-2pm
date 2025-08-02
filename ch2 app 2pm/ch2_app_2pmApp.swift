15f8a51ca478363b188f2e3e056fe6d2bda9a2ef
qin en <qinentoh@gmail.com>
2025-08-02 16:27:20 +0800

    Initial Commit

---
+++ ch2 app 2pm/ch2_app_2pmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ch2_app_2pmApp.swift
+//  ch2 app 2pm
+//
+//  Created by T Krobot on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ch2_app_2pmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
